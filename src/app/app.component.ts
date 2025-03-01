f184699e7e8a0163355e42a6254f843cda8e99bb
HeshamAbdElmonsef <97709584+HeshamAbdElmonsef@users.noreply.github.com>
2025-03-01 20:34:20 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'XPEtest';
+  title = 'Candidate List';
+  baseURL = 'http://myxpetask.runasp.net/api/Candidates/Get-All';
+  Candidate: any[] = [];
+  selectedFile: File | null = null;
+  uploadMessage: string = '';
+  loading = false;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any): void {
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+    }
+  }
+
+  onUpload(): void {
+    if (!this.selectedFile) {
+      this.uploadMessage = 'Please select a file first.';
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
+
+    this.http.post('http://myxpetask.runasp.net/api/Candidates/upload', formData)
+      .subscribe({
+        next: () => {
+          this.uploadMessage = 'File uploaded successfully!';
+        },
+        error: () => {
+          this.uploadMessage = 'Error uploading file.';
+        }
+      });
+  }
+
+  loadCandidates(): void {
+    this.loading = true;
+    this.http.get<any[]>(this.baseURL).subscribe({
+      next: (data) => {
+        this.Candidate = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching candidates:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule,provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
